7ca5f3e679dbec6cef51111f90ce03f048b77c6b
Kris Robinson <robinskr@wwt.com>
2021-12-05 20:31:37 -0600

    main - Added User Profile component and test

---
+++ web/src/components/UserProfile.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const UserProfile = () => {
+    const [error, setError] = useState(null);
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/Profile`);
+            setName(data.name);
+          } catch (err) {
+            setError(err);
+          }
+        };
+    
+        fetchData();
+      }, []);
+
+    if (error) {
+        return <div>Oops! Could not fetch practice member profile.</div>;
+    }
+
+    return (
+        <div>
+          <h2>{name}</h2>
+        </div>
+      );
+}
+
+export default UserProfile;
