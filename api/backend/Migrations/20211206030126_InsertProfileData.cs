01ddadf124d47c7e257f79921abe599569b32cb1
Kris Robinson <robinskr@wwt.com>
2021-12-07 14:55:36 -0600

    main - Added migrations

---
+++ api/backend/Migrations/20211206030126_InsertProfileData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    public partial class InsertProfileData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Users", "Name", "Kris Robinson");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("Users", "Name", "Kris Robinson");
+
+        }
+    }
+}
