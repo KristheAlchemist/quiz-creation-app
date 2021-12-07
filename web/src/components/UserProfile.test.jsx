f342fdf8114490f0ce7ccc0ac6d3852723d9b1a8
Kris Robinson <robinskr@wwt.com>
2021-12-07 13:46:37 -0600

    main - Added UserProfile component and test

---
+++ web/src/components/UserProfile.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { render, screen } from '@testing-library/react';
+import UserProfile from './UserProfile';
+
+
+const userProfileDetails = {
+    name: 'Kris Robinson',
+  };
+
+  test('renders practice member name', async () => {
+    const mockApi = new MockAdapter(axios);
+    mockApi.onGet(`${process.env.REACT_APP_BASE_API}/api/Profile`).reply(200, userProfileDetails);
+    render(<UserProfile />);
+    expect(await screen.findByText('Kris Robinson')).toBeInTheDocument();
+  });
