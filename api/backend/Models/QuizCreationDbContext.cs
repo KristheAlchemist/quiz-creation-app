0381ded1ccd53bd1850329e38969bcafb7fdc564
Kris Robinson <robinskr@wwt.com>
2021-12-07 14:52:59 -0600

    main - Added DbContext and migrations

---
+++ api/backend/Models/QuizCreationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace backend.Models
+{
+    public class QuizCreationDbContext : DbContext
+    {
+        public QuizCreationDbContext() { }
+        public QuizCreationDbContext(DbContextOptions<QuizCreationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
