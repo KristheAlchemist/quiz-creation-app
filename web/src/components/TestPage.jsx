8bf09a31dd3bf395e819de3a0146f46dc1940359
Kris Robinson <robinskr@wwt.com>
2021-12-08 11:51:05 -0600

    main - Added loading state for TestPage & UserProfile

--- web/src/components/TestPage.jsx
+++ web/src/components/TestPage.jsx
@@ -4,15 +4,18 @@ import React, { useEffect, useState } from 'react';
 const TestPage = () => {
   const [error, setError] = useState(null);
   const [title, setTitle] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/Test`);
         setTitle(data.title);
       } catch (err) {
         setError(err);
       }
+      setLoading(false);
     };
 
     fetchData();
@@ -20,6 +23,10 @@ const TestPage = () => {
 
   if (error) {
     return <div>Oops! Could not fetch the test page.</div>;
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
 
   return (
--- web/src/components/UserProfile.jsx
+++ web/src/components/UserProfile.jsx
@@ -4,15 +4,18 @@ import React, { useEffect, useState } from 'react';
 const UserProfile = () => {
   const [error, setError] = useState(null);
   const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/User`);
         setName(data.name);
       } catch (err) {
         setError(err);
       }
+      setLoading(false);
     };
 
     fetchData();
@@ -20,6 +23,10 @@ const UserProfile = () => {
 
   if (error) {
     return <div>Oops! Could not fetch practice member profile.</div>;
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
 
   return (
