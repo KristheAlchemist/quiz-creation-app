66bd403f9b7523fda783558d839e564959bd3b86
Kris Robinson <robinskr@wwt.com>
2021-12-08 16:48:04 -0600

    main - Added QuestionController and test

---
+++ api/backend.Tests/Controllers/QuestionControllerTest.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Controllers;
+using backend.Models;
+using backend.Tests.Utils;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace backend.Tests.Controllers
+{
+    public class QuestionControllerTest : IAsyncLifetime
+    {
+
+        private QuestionController testObject;
+        private QuizCreationDbContext db;
+
+        public async Task InitializeAsync()
+        {
+            db = await TestUtils.GetTestDbContext();
+            testObject = new QuestionController(db, new Mock<ILogger<QuestionController>>().Object);
+        }
+
+        public async Task DisposeAsync()
+        {
+            await db.DisposeAsync();
+        }
+
+        public class GetQuestion : QuestionControllerTest
+        {
+
+            [Fact]
+            public async void WhenDbConnection_ReturnsOkObjectContainingTest()
+            {
+                var response = await testObject.Get();
+
+                response.Should().BeOfType<OkObjectResult>();
+                var result = (response as OkObjectResult).Value as Question;
+                result.Text.Should().Be(TestUtils.QUESTION_TEXT);
+            }
+
+            [Fact]
+            public async void WhenNoQuestion_ReturnsNotFound()
+            {
+                db.Questions.Remove(db.Questions.First());
+                await db.SaveChangesAsync();
+
+                var request = TestUtils.CreateMockRequest("GET");
+
+                var response = await testObject.Get();
+
+                response.Should().BeOfType<NotFoundResult>();
+            }
+
+            [Fact]
+            public async void WhenAnErrorOccursUsingDataBase_ThrowsError()
+            {
+                var mockDb = new Mock<QuizCreationDbContext>();
+
+                mockDb.Setup(x => x.Questions).Throws(new Exception("Something Broke"));
+
+                var testObject = new QuestionController(mockDb.Object, new Mock<ILogger<QuestionController>>().Object);
+
+                var exception = await Assert.ThrowsAsync<Exception>(() => testObject.Get());
+
+                exception.Message.Should().Be("Something Broke");
+            }
+        }
+    }
+}
---
+++ api/backend/Controllers/QuestionController.cs
@@ -0,0 +1,43 @@
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuestionController : ControllerBase
+    {
+        private readonly QuizCreationDbContext _db;
+        private readonly ILogger<QuestionController> _logger;
+
+        public QuestionController(QuizCreationDbContext db, ILogger<QuestionController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            Question? question = null;
+
+            try
+            {
+                question = await _db.Questions.FirstOrDefaultAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical($"SQL Read error. It is likely that there is no database connection established. ${e.Message}");
+                throw;
+            }
+
+            if (question == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(question);
+        }
+    }
+}
