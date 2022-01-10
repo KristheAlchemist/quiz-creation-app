476d649fe423d77c1449464be54571628d57d59d
Kris Robinson <robinskr@wwt.com>
2022-01-09 22:34:16 -0600

    answering - Added BrowserRouter to fix routing error

--- web/src/App.test.jsx
+++ web/src/App.test.jsx
@@ -1,8 +1,14 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test('renders Quiz Creation App text', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Quiz Creation App/);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders Quiz Creation App text', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+  const linkElement = screen.getByText(/Quiz Creation App/);
+  expect(linkElement).toBeInTheDocument();
+});
