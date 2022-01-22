2cbf33ca9a220d090f0efb3f14d3811ab0a2ee79
Kris Robinson <robinskr@wwt.com>
2022-01-22 04:15:56 -0600

    answering - Added UserQuiz and UserAnswer ViewModel responses

--- api/backend/Models/ViewModels/UserResponse.cs
+++ api/backend/Models/ViewModels/UserResponse.cs
@@ -6,5 +6,20 @@ namespace backend.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public virtual IEnumerable<UserQuizResponse> UserQuizzes { get; set; }
+    }
+
+    public class UserQuizResponse
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int QuizId { get; set; }
+        public virtual IEnumerable<UserAnswerResponse> UserAnswers { get; set; }
+    }
+
+    public class UserAnswerResponse
+    {
+        public int Id { get; set; }
+        public string Answer { get; set; }
     }
 }
