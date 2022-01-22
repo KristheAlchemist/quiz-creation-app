f1fbe5f24e75f4a43d7f8e6814b667217b240a0a
Kris Robinson <robinskr@wwt.com>
2022-01-22 14:45:05 -0600

    answering - Made relational properties virtual

--- api/backend/Models/Question.cs
+++ api/backend/Models/Question.cs
@@ -8,7 +8,7 @@ namespace backend.Models
         public string Text { get; set; }
         public string CorrectAnswer { get; set; }
         public int QuestionTypeId { get; set; }
-        public ICollection<Choice> Choices { get; set; }
+        public virtual ICollection<Choice> Choices { get; set; }
 
         public virtual QuestionType QuestionType { get; set; }
     }
--- api/backend/Models/UserQuiz.cs
+++ api/backend/Models/UserQuiz.cs
@@ -7,8 +7,8 @@ namespace backend.Models
         public int Id { get; set; }
         public int UserId { get; set; }
         public int QuizId { get; set; }
-        public ICollection<UserAnswer> UserAnswers { get; set; }
 
+        public virtual ICollection<UserAnswer> UserAnswers { get; set; }
         public virtual User User { get; set; }
         public virtual Quiz Quiz { get; set; }
     }
