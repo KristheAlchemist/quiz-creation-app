28d13805a4df056622c2e5da4380ba1abf4ffb6f
Kris Robinson <robinskr@wwt.com>
2022-02-11 11:10:18 -0600

    answering - Simplified ShortAnswer component

--- web/src/components/QuestionTypes/ShortAnswer.jsx
+++ web/src/components/QuestionTypes/ShortAnswer.jsx
@@ -1,43 +1,13 @@
-// import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-const ShortAnswer = () => {
-  // const [answer, setAnswer] = useState('');
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        // const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/Question`);
-        // setAnswer(data.answer);
-      } catch (err) {
-        setError(err);
-      }
-      setLoading(false);
-    };
-
-    fetchData();
-  }, []);
-
-  if (error) {
-    return <div>Oops! Could not fetch the input box.</div>;
-  }
-
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-
-  return (
-    <div>
-      <form action="" method="post">
-        <p>Please answer here:</p>
-        <input type="text" />
-        <input type="submit" />
-      </form>
-    </div>
-  );
-};
+const ShortAnswer = () => (
+  <div>
+    <label>
+      Please answer here:
+      <br />
+      <input type="text" />
+    </label>
+  </div>
+);
 
 export default ShortAnswer;
