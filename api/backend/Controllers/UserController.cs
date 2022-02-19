6f07e3e866cebd677ffe40e8c2b74c6f87072665
Kris Robinson <robinskr@wwt.com>
2022-02-19 05:43:26 -0600

    answering - Added GetById request for UserController

--- api/backend/Controllers/UserController.cs
+++ api/backend/Controllers/UserController.cs
@@ -1,9 +1,56 @@
 using backend.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers
 {
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly QuizCreationDbContext _db;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(QuizCreationDbContext db, ILogger<UsersController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var users = await _db.Users
+                    .ToListAsync();
+
+                if (users == null)
+                {
+                    return new NotFoundResult();
+                }
+
+                var usersResponse = users.Select(user =>
+                    new UserResponse
+                    {
+                        Id = user.Id,
+                        Name = user.Name,
+                    }
+                );
+
+                return new OkObjectResult(usersResponse);
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical($"SQL Read error. It is likely that there is no database connection established. ${e.Message}");
+                throw;
+            }
+        }
+    }
+
+    [AllowAnonymous]
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
@@ -17,27 +64,37 @@ namespace backend.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
         {
-            User user = null;
-
             try
             {
-                user = await _db.Users.FirstOrDefaultAsync();
+                var user = await _db.Users
+                    .Include(u => u.UserQuizzes)
+                    .ThenInclude(us => us.Quiz)
+                    .FirstOrDefaultAsync(u => u.Id == id);
+
+                if (user == null)
+                {
+                    return new NotFoundResult();
+                }
+
+                var userResponse = new UserResponse
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                };
+
+                return new OkObjectResult(userResponse);
             }
+
+
+
             catch (Exception e)
             {
                 _logger.LogCritical($"SQL Read error. It is likely that there is no database connection established. ${e.Message}");
                 throw;
             }
-
-            if (user == null)
-            {
-                return new NotFoundResult();
-            }
-
-            return new OkObjectResult(user);
         }
     }
 }
