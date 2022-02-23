cb28b83b826a49e04152610a20060b07da1df8c4
Kris Robinson <robinskr@wwt.com>
2022-02-23 14:03:54 -0600

    answering - Split user routes

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,30 +1,34 @@
 import './App.css';
 import React from 'react';
 import { Route, Routes } from 'react-router';
-import UserProfile from './components/UserProfile';
-import UserList from './components/UserList';
+import TeacherProfile from './components/TeacherProfile';
+import TeacherList from './components/TeacherList';
+import StudentProfile from './components/StudentProfile';
+import StudentList from './components/StudentList';
 import QuizPage from './components/QuizPage';
 import QuizListPage from './components/QuizListPage';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h3>
           Quiz Creation App
         </h3>
         <h4>
-          <a href="/users" className="App-link">Users</a>
+          <a href="/teachers" className="App-link">Teachers</a>
+          |   |
+          <a href="/students" className="App-link">Students</a>
           |   |
           <a href="/quizzes" className="App-link">Quizzes</a>
         </h4>
       </header>
       <Routes>
         <Route path="/" element={<QuizListPage />} />
-        <Route path="/users" element={<UserList />} />
-        <Route path="/user/:id" element={<UserProfile />} />
+        <Route path="/teachers" element={<TeacherList />} />
+        <Route path="/teacher/:id" element={<TeacherProfile />} />
+        <Route path="/students" element={<StudentList />} />
+        <Route path="/student/:id" element={<StudentProfile />} />
         <Route path="/quizzes" element={<QuizListPage />} />
         <Route path="/quiz/:id" element={<QuizPage />} />
       </Routes>
