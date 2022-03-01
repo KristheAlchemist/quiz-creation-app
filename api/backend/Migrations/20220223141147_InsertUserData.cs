c3e702428290f7d4b452d59871ca8e4e88aa9110
Kris Robinson <robinskr@wwt.com>
2022-03-01 15:59:27 -0600

    answering - Temporally muted student migration

--- api/backend/Migrations/20220223141147_InsertUserData.cs
+++ api/backend/Migrations/20220223141147_InsertUserData.cs
@@ -8,13 +8,12 @@ namespace backend.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // migrationBuilder.InsertData("Students", "Name", "Kris Robinson");
-            migrationBuilder.InsertData("Students", new string[] { "Name", "Email" }, new object[] { "Kris Robinson", "kris@wwt.com" });
+            // migrationBuilder.InsertData("Students", new string[] { "Name", "Email" }, new object[] { "Kris Robinson", "kris@wwt.com" });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DeleteData("Students", "Name", "Kris Robinson");
+            migrationBuilder.DeleteData("Students", new string[] { "Name", "Email" }, new object[] { "Kris Robinson", "kris@wwt.com" });
         }
     }
 }
