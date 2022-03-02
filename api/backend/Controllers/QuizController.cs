49146b5af8239e5ac4b70b7227df2c2656103d0a
Kris Robinson <robinskr@wwt.com>
2022-03-02 11:12:22 -0600

    answering - Added choices to get responses

--- api/backend/Controllers/QuizController.cs
+++ api/backend/Controllers/QuizController.cs
@@ -66,8 +66,7 @@ namespace backend.Controllers
                 var quiz = await _db.Quizzes
                     .Include(q => q.QuizQuestions)
                     .ThenInclude(qq => qq.Question)
-                    .Include(q => q.QuizQuestions)
-                    .ThenInclude(qq => qq.Question)
+                    .ThenInclude(q => q.Choices)
                     .FirstOrDefaultAsync(q => q.Id == id);
 
                 if (quiz == null)
@@ -84,7 +83,15 @@ namespace backend.Controllers
                         {
                             Id = qq.Question.Id,
                             Text = qq.Question.Text,
-                            QuestionType = qq.Question.QuestionType
+                            QuestionType = qq.Question.QuestionType,
+                            Choices = qq.Question.Choices.Select(c =>
+                               new ChoiceResponse
+                               {
+                                   Id = c.Id,
+                                   Text = c.Text,
+                                   QuestionId = c.QuestionId
+                               }
+                           )
                         }
                     )
                 };
@@ -131,6 +138,7 @@ namespace backend.Controllers
             var addedQuiz = await _db.Quizzes
             .Include(s => s.QuizQuestions)
             .ThenInclude(qq => qq.Question)
+            .ThenInclude(q => q.Choices)
             .SingleAsync(s => s.Id == quizToAdd.Id);
 
             return new CreatedResult("api/Quizzes" + quizToAdd.Id, new QuizResponse(addedQuiz));
