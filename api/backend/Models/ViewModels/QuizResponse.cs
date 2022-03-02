66d7a2123c8f6c6073c9f12eb9788be816f518ee
Kris Robinson <robinskr@wwt.com>
2022-03-02 12:11:43 -0600

    answering - Added overloads

--- api/backend/Models/ViewModels/QuizResponse.cs
+++ api/backend/Models/ViewModels/QuizResponse.cs
@@ -20,6 +20,20 @@ namespace backend.Models
         public string Text { get; set; }
         public QuestionType QuestionType { get; set; }
         public IEnumerable<ChoiceResponse> Choices { get; set; }
+
+        public QuestionResponse() { }
+        public QuestionResponse(Question question)
+        {
+            Id = question.Id;
+            Text = question.Text;
+            Choices = question.Choices.Select(choice =>
+                new ChoiceResponse
+                {
+                    Id = choice.Id,
+                    Text = choice.Text,
+
+                });
+        }
     }
 
     public class ChoiceResponse
